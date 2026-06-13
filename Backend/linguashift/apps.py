fc1a8fbc112cfde7b59a0d1fd21eced39a4dd42d
Eric Valdez <ercvldez@gmail.com>
2026-06-13 16:27:54 -0700

    Created frontend and backend directories

---
+++ Backend/linguashift/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LinguashiftConfig(AppConfig):
+    name = 'linguashift'
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1>LinguaShift</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
